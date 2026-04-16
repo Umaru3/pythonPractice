469d7e0e19ca4cfd4348489eeadea22b323de1c9
VEV\justi <justincastro105@gmail.com>
2026-04-16 18:16:05 +0800

    Branch-001: [FEATURE][ALL] - Day3

---
+++ pythonTry/day_3/operators.py
@@ -0,0 +1,39 @@
+# Day 3
+import math
+
+age = 26
+height = 1.60
+complexNumber = 1999 - 2026
+
+#Triangle calculations
+# baseTriangle = input('Base of triangle: ')
+# heightTriangle = input('Height of triangle: ')
+# areaOfTriangle = 0.5 * float(baseTriangle) * float(heightTriangle)
+# print("Area of triangle: ", areaOfTriangle)
+
+#Triangle calculations
+# sideA = input('Side A of triangle: ')
+# sideB = input('Side B of triangle: ')
+# sideC = input('Side C of triangle: ')
+# perimeterOfTriangle = float(sideA) + float(sideB) + float(sideC)
+# print("Perimeter of triangle: ", perimeterOfTriangle)
+
+#Rectangle calculations
+# rectangleLength = input('Length of rectangle: ')
+# rectangleWidth = input('Width of rectangle: ')
+# areaOfRectangle = float(rectangleLength) * float(rectangleWidth)
+# perimeterOfRectangle = 2 * (float(rectangleLength) + float(rectangleWidth))
+# print("Area of rectangle: ", areaOfRectangle)
+# print("Perimeter of rectangle: ", perimeterOfRectangle)
+
+#Circle calculations
+# radiusCircle = input('Radius of circle: ')
+# areaOfCircle = math.pi * float(radiusCircle) ** 2
+# circumferenceOfCircle = 2 * math.pi * float(radiusCircle)
+# print("Area of circle: ", areaOfCircle)
+# print("Circumference of circle: ", circumferenceOfCircle)
+
+#Slope Calculations
+xI = 2
+slope = (2 * xI - 2)
+print("Slope: ", slope)
